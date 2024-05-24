52295d75a811b3dcc81478b2db0974bf695be3c3
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2024-05-24 23:01:08 +0300

    delete-comp

--- tests/setup.js
+++ tests/setup.js
@@ -1,9 +1,5 @@
 import Vue from 'vue';
-import VueCompositionApi from '@vue/composition-api'; // If you're using Composition API
 import { config } from '@vue/test-utils';
-
-// Install any plugins you're using in your Vue app
-Vue.use(VueCompositionApi);
 
 // Suppress warnings about not using the Vue instance in the tests
 config.showDeprecationWarnings = false;
